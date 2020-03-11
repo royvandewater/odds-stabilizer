e84b6239ec5701577224a0aa0544f1daf87663f0
Roy van de Water <roy.vwater@gmail.com>
2020-03-11 07:19:20 -0700

    Add betting & calculated outcomes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const houseWinningsForA = bets => bets.b - bets.a
+const houseWinningsForB = bets => bets.a - bets.b
+
 function App() {
+  const [bets, setBets] = React.useState({a: 0, b: 0})
+
+  const betOnA = () => setBets({...bets, a: bets.a + 1})
+  const betOnB = () => setBets({...bets, b: bets.b + 1})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Odds Stabilizer</h1>
       </header>
+      <main>
+        <section className="App-bet-section">
+          <h2>Bet</h2>
+          <div className="App-bet-grid">
+            <button className="App-button" onClick={betOnA}>A</button>
+            <button className="App-button" onClick={betOnB}>B</button>
+            <p>${bets.a}</p>
+            <p>${bets.b}</p>
+          </div>
+        </section>
+        <section>
+          <h2>Outcomes</h2>
+          <dl>
+            <dt>If A wins the house wins:</dt>
+            <dd>${houseWinningsForA(bets)}</dd>
+
+            <dt>If B wins the house has:</dt>
+            <dd>${houseWinningsForB(bets)}</dd>
+          </dl>
+        </section>
+      </main>
     </div>
   );
 }
